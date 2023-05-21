ed110beedfc267fcb8b54abd3260363abf8316b7
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-05-21 09:36:18 +0000

    order

--- broom/src/main/java/ch/fhnw/broom/data/domain/Order.java
+++ broom/src/main/java/ch/fhnw/broom/data/domain/Order.java
@@ -1,5 +1,15 @@
 package ch.fhnw.broom.data.domain;
 
+import java.lang.annotation.Inherited;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     
 }
