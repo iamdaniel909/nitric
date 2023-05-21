6942c637acbfceb03c6c5e3970936b90e41b5386
pand000 <127597048+pand000@users.noreply.github.com>
2023-05-21 09:38:57 +0000

    studentclass

--- broom/src/main/java/ch/fhnw/broom/data/domain/Student.java
+++ broom/src/main/java/ch/fhnw/broom/data/domain/Student.java
@@ -33,34 +33,110 @@ public class Student {
 
     @Column(name = "SchoolYear")
     private String schoolyear;
-    
-    @Id
-    private String email;
 
-    private String name;
+    @OneToMany
+    private Order order;
 
     @JsonIgnore
+    private List<Order> orders;
     @OneToMany(mappedBy = "student")
-    private List<Booking> bookings;
+    
 
     public Student() {
     }
 
-    public Student(String email, String name) {
-        this.email = email;
-        this.name = name;
+    public Student(String fname, String lname, String birthdate, String adress, String house, String schoolyear) {
+        this.fname = fname;
+        this.lname = lname;
+        this.birthdate = birthdate;
+        this.adress = adress;
+        this.house = house;
+        this.schoolyear = schoolyear;
     }
 
-    public String getEmail() {
-        return email;
+    public Long getId() {
+        return id;
+    }
+    
+    public String getFname() {
+        return fname;
     }
 
-    public String getName() {
-        return name;
+    public String getLname() {
+        return lname;
     }
 
-    public List<Booking> getBookings() {
-        return bookings;
+    public String getBirthdate() {
+        return birthdate;
     }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public String getSchoolyear() {
+        return schoolyear;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+    public List<Order> getOrders() {
+        return orders;
+    }
+    public void setId(Long id) {
+        this.id = id;
+
+    }
+    public void setFname(String fname) {
+        this.fname = fname;
+
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+
+    }
+
+    public void setSchoolyear(String schoolyear) {
+        this.schoolyear = schoolyear;
+
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student [adress=" + adress + ", birthdate=" + birthdate + ", fname=" + fname + ", house=" + house
+                + ", id=" + id + ", lname=" + lname + ", order=" + order + ", orders=" + orders + ", schoolyear="
+                + schoolyear + "]";
+    }
+
 
 }
