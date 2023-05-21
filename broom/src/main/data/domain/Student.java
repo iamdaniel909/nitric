28069c200a24f61da3adc22b3f7ed864b98f9c3f
pand000 <127597048+pand000@users.noreply.github.com>
2023-05-21 08:51:02 +0000

    stduent

--- broom/src/main/data/domain/Student.java
+++ broom/src/main/data/domain/Student.java
@@ -3,3 +3,61 @@ package ch.fhnw.broom.data.domain;
 import jakarta.persistence.*;
 import java.util.List;
 
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+public class Student [
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String token;
+    private String tokenExpiration;
+    private String tokenCreation;
+    private String tokenUsage;
+    private String tokenValid;
+    private String tokenInvalid;
+    private String tokenExpired;
+    private String tokenUsed;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+    private String tokenNotUsed;
+    private String tokenNotValid;
+    private String tokenNotInvalid;
+    private String tokenNotExpired;
+]
+
