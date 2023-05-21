118ee673e2c252219000ab8a27cfe901c19ae24d
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-05-21 09:44:11 +0000

    getters setters

--- broom/src/main/java/ch/fhnw/broom/data/domain/Order.java
+++ broom/src/main/java/ch/fhnw/broom/data/domain/Order.java
@@ -11,5 +11,38 @@ public class Order {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    
+    @Column(name = "order_amount")
+    private int orderAmount;
+
+    @Column(name = "order_date")
+    private Date orderDate;
+
+    @ManyToMany
+    private Student student;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(int amount) {
+        this.orderAmount = amount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date date) {
+        this.orderDate = date;
+    }
+
+
 }
