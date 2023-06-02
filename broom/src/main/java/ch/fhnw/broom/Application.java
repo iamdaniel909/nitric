244a7261766211ee5a7373eead288f70f4071c76
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-06-02 13:17:22 +0000

    import statements

--- broom/src/main/java/ch/fhnw/broom/Application.java
+++ broom/src/main/java/ch/fhnw/broom/Application.java
@@ -3,11 +3,22 @@ package ch.fhnw.broom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.fhnw.broom.business.service.CatalogueService;
+
 @SpringBootApplication
+@RestController
 public class Application {
+
+	@Autowired
+	private CatalogueService catalogueService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping(value="/")
+	public String getWelcomeString() {
+		return "Welcome to ye ole broom shoppe!";
 	}
 
 }
