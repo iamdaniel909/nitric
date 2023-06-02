41bca23c48a0ff1701a04787dd37509da742dded
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-06-02 12:52:18 +0000

    business service

--- broom/src/main/java/ch/fhnw/broom/business/service/CatalogueService.java
+++ broom/src/main/java/ch/fhnw/broom/business/service/CatalogueService.java
@@ -27,6 +27,35 @@ public class CatalogueService {
         return productList;
     }
 
-    
+    public Product addProduct(Product product) throws Exeption {
+        if (product.getProductName() != null) {
+            if (productRepository.findByProductName(product.getProductName()) == null) 
+                return productRepository.save(product);
+            throw new Exeption("Product " + product.getProductName() + " already exists");
+        }
+        throw new Exeption("Invalid product name");
+    }
 
+    //Business Logic to get current offer according to the Schoolyear of the Student
+    private String getCurrentOffer(String house) {
+        String currentOffer = "No special offer";
+        if("Griffindor".equalsIgnoreCase(house))
+            currentOffer = "10% discount on all brooms";
+        else if("Slytherin".equalsIgnoreCase(house))
+            currentOffer = "15% discount on all brooms";
+        else if("Hufflepuff".equalsIgnoreCase(house))
+            currentOffer = "20% discount on all brooms";
+        else if("Ravenclaw".equalsIgnoreCase(house))
+            currentOffer = "25% discount on all brooms";
+    }
+
+    public Catalogue getCatalogueByHouse(String house) {
+        String currentOffer = getCurrentOffer(house);
+        List<Product> productList = getAllProducts();
+        Catalogue catalogue = new Catalogue();
+        catalogue.setProductList(productList);
+        catalogue.setCurrentOffer(currentOffer);
+        return catalogue;
+    }
 }
+
